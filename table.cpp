ef8e62535c36ddd4bda705b9e47b737a9d4a4107
akriticode09 <158453972+akriticode09@users.noreply.github.com>
2024-05-28 23:45:41 +0530

    print table of a number

---
+++ table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cout<<"enter a number"<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        cout<<(n*i)<<endl;
+    }
+    
+
+}
