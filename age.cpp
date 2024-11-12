7335a686bf49e70dfcdcfb10bb509761b37bf79f
akriticode09 <158453972+akriticode09@users.noreply.github.com>
2024-11-12 17:59:25 +0530

    checking age for voting

---
+++ age.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cin>>age;
+    if(age>=18)
+    {
+        cout<<"can vote"<<endl;
+
+    }
+    else{
+        cout<<"cannot vote"<<endl;
+    }
+}
