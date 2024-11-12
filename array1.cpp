1b4d500fe78c4063d39ecb9dd727c6a48d79d74c
akriticode09 <158453972+akriticode09@users.noreply.github.com>
2024-11-12 17:51:00 +0530

    printing of an array

---
+++ array1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int marks[5],i;
+   cout<<"enter the elements of array "<<endl;
+   for( i=0;i<5;i++)
+   {
+    cin>>marks[i];
+   }
+   int topmarks[] = {100,99,98}; 
+   cout<<topmarks[1]<<endl;
+   cout<<marks[0]<<endl;
+}
