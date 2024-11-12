c1764515c7c3b5fa460d40146bb5c71f0aea8a09
akriticode09 <158453972+akriticode09@users.noreply.github.com>
2024-11-12 15:19:04 +0530

    grad

---
+++ grad.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int price;
+  cout << "the price of the item is "<< endl;
+  cin >> price;
+  cout << "you have to pay "<< price <<endl;
+   return 0;
+}
