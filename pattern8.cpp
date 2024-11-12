f9857613bf4e4187149cb2d6c944758fb61111a1
akriticode09 <158453972+akriticode09@users.noreply.github.com>
2024-11-12 15:03:54 +0530

    pattern printing

---
+++ pattern8.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 4,i,j,num =1;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cout<<num;
+            num++;
+        }
+        cout<<endl;
+    }
+}
